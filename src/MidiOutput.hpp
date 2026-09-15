6ed1d82a255db65c76995706b88b6e237ccdb708
Sei K. <83477269+AZero13@users.noreply.github.com>
2026-09-15 16:52:57 -0400

    Fix MIDI matches

--- src/MidiOutput.hpp
+++ src/MidiOutput.hpp
@@ -135,19 +135,14 @@ struct MidiOutput : MidiTimer
 
     static u32 Ntohl(u32 val)
     {
-        union {
-            u32 value;
-            u8 bytes[4];
-        } in, out;
+        u8 tmp[4];
 
-        in.value = val;
+        tmp[0] = ((u8 *)&val)[3];
+        tmp[1] = ((u8 *)&val)[2];
+        tmp[2] = ((u8 *)&val)[1];
+        tmp[3] = ((u8 *)&val)[0];
 
-        out.bytes[0] = in.bytes[3];
-        out.bytes[1] = in.bytes[2];
-        out.bytes[2] = in.bytes[1];
-        out.bytes[3] = in.bytes[0];
-
-        return out.value;
+        return *(const u32 *)(&tmp);
     }
 
     MIDIHDR *midiHeaders[32];
